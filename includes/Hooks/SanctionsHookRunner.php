e8b6923d24162517f364158c7aa4fabc446c95d8
lens0021 <lorentz0021@gmail.com>
2024-06-30 02:59:51 +0900

    RenameUser is merged into core
    Change-Id: I2068534ad56b129d67dd6d92ddc9cc5c47384cdf

--- includes/Hooks/SanctionsHookRunner.php
+++ includes/Hooks/SanctionsHookRunner.php
@@ -5,7 +5,7 @@ namespace MediaWiki\Extension\Sanctions\Hooks;
 use MediaWiki\HookContainer\HookContainer;
 
 class SanctionsHookRunner implements
-	\MediaWiki\Extension\Renameuser\Hook\RenameUserAbortHook
+	\MediaWiki\Renameuser\Hook\RenameUserAbortHook
 {
 	/**
 	 * @var HookContainer
