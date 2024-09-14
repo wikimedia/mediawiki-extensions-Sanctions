084b1bb20669074224a770885b0f5e0587e6f539
lens0021 <lorentz0021@gmail.com>
2024-09-14 23:46:26 +0900

    fix: RequestContext should not be cloned
    Bug: T373863
    Change-Id: I4f5e775876c2f72362c6c7783cb9b9e3e4789d0f

--- includes/FlowUtil.php
+++ includes/FlowUtil.php
@@ -13,6 +13,7 @@ use Flow\Model\PostSummary;
 use Flow\Model\UUID;
 use Flow\WorkflowLoaderFactory;
 use IDBAccessObject;
+use MediaWiki\Context\DerivativeContext;
 use MediaWiki\MediaWikiServices;
 use Psr\Log\NullLogger;
 use RequestContext;
@@ -147,7 +148,7 @@ class FlowUtil {
 			];
 
 			$blocksToCommit = $loader->handleSubmit(
-				clone RequestContext::getMain(),
+				new DerivativeContext( RequestContext::getMain() ),
 				$action,
 				$params
 			);
